88fd0b4171ceca7047ac4c2a0791af2a27ec6de2
AaronLyons1 <lyonsaaron02@gmail.com>
2022-03-01 19:42:47 +0000

    add player scores and start checkWinner funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@
 // Get the button elements and add event listeners to them
 
 let button = document.getElementsByClassName("btn")
-let pcScore = getElementsByClassName("pc-score")
-let playerScore = getElementsByClassName("ply-score")
-let choices = ["paper","rock","scissors"]
+let pcScore = document.getElementsByClassName("pc-score")
+let playerImage = document.getElementById("ply-image")
+let pcImage = document.getElementById("pc-image")
+let playerScore = document.getElementsByClassName("ply-score")
+let options = ["paper","rock","scissors"]
 
 /**
  * Eventlisteners here!
@@ -16,3 +18,27 @@ for( let button of buttons){
     } );
 }
 
+function runGame(gamerChoice){
+
+    playerImage.src = `assets/images/${options[gamerChoice]}`;
+    playerImage.alt = options[gamerChoice];
+
+    let pcChoice = Math.floor(Math.random() * 3)
+
+    pcImage.src = `assets/images/${options[gamerChoice]}`;
+    pcImage.alt = options[gamerChoice];
+
+    let result = checkWinner(options[pcChoice], options[playerChoice]);
+
+    updateScore(result);
+}
+
+function checkWinner(){
+    let currentGame = `${pcChoice} against ${playerChoice}`;
+
+    if (pcScore === playerScore){
+        alert(`${currentGame} is a Draw!`)
+        return
+    }
+    
+}
