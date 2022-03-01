29e4371e72537a01af1539a1b85a98244d6f0f02
AaronLyons1 <lyonsaaron02@gmail.com>
2022-03-01 22:22:44 +0000

    fix majority of problems now almost all code work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,74 +6,74 @@ let pcScore = document.getElementsByClassName("pc-score");
 let playerImage = document.getElementById("ply-image");
 let pcImage = document.getElementById("pc-image");
 let playerScore = document.getElementsByClassName("ply-score");
-let options = ["rock","paper","scissors"];
-const pcChoice = document.getAttribute("data-choice");
-const gamerChoice = document.getAttribute("data-choice");
+let options = ["rock", "paper", "scissors"];
+const pcChoice = null;
 
 /**
  * Eventlisteners here!
  */
-for( let button of buttons){
-    button.addEventListener("click", function (){
-        let gamerChoice = this.getAttribute("data-choice");
-        runGame(gamerChoice);
-    } );
+for (let button of buttons) {
+	button.addEventListener("click", function() {
+		let gamerChoice = this.getAttribute("data-choice");
+		runGame(gamerChoice);
+	});
 }
 
-function runGame(gamerChoice){
 
-    playerImage.src = `assets/images/${options[gamerChoice]}`;
-    playerImage.alt = options[gamerChoice];
+function runGame(gamerChoice) {
 
-    let pcChoice = Math.floor(Math.random() * 3);
 
-    pcImage.src = `assets/images/${options[gamerChoice]}`;
-    pcImage.alt = options[gamerChoice];
+	let pcChoice = Math.floor(Math.random() * 3);
 
-    let result = checkWinner(options[pcChoice], options[gamerChoice]);
+	let result = checkWinner(options[pcChoice], gamerChoice);
 
-    updateScore(result);
+	updateScore(result);
 }
 
-function checkWinner(){
-    let currentGame = `${pcChoice} against ${gamerChoice}`;
+function checkWinner(pcChoice, gamerChoice) {
+	let currentGame = `${pcChoice} against ${gamerChoice}`;
 
-    //Check if the game is a tie
-    if (pcScore === playerScore){
-        alert(`${currentGame} is a Draw!`);
-        return;
-    }
+	//Check if the game is a tie
+	if (pcScore === playerScore) {
+		alert(`${currentGame} is a Draw!`);
+		return;
+	}
 
 
-    //rock
-    if(gamerChoice === "rock"){
-        if(pcChoice === "scissors")
-        alert(`${currentGame} = You Have Won! Yay`);
-    } else {
-        alert(`${currentGame} = Dam, the Computer was just better`);
-    }
+	//rock
+	if (gamerChoice === "rock") {
+		if (pcChoice === "scissors")
+			alert(`${currentGame} = You Have Won! Yay`);
+		else
+			alert(`${currentGame} = Dam, the Computer was just better`);
 
-    //paper
-    if(gamerChoice === "paper"){
-        if(pcChoice === "rock")
-        alert(`${currentGame} = You Have Won! Yay`);
-    } else {
-        alert(`${currentGame} = Dam, the Computer was just better`);
-    }
-
-    //scissors
-    if (gamerChoice === "scissors"){
-        if(pcChoice === "paper")
-        alert(`${currentGame} = You Have Won! Yay`);
-    } else {
-        alert(`${currentGame} = Dam, the Computer was just better`);
-    }
+	}
 }
 
-function updateScore(){
-    if (alert === "`${currentGame} = Dam, the Computer was just better`"){
-        ++pcScore;
-    }else {
-        ++playerScore;
-    }
+//paper
+if (gamerChoice === "paper") {
+	if (pcChoice === "rock")
+		alert(`${currentGame} = You Have Won! Yay`);
+	else
+		alert(`${currentGame} = Dam, the Computer was just better`);
+
+}
+
+
+//scissors
+if (gamerChoice === "scissors") {
+	if (pcChoice === "paper")
+		alert(`${currentGame} = You Have Won! Yay`);
+	else
+		alert(`${currentGame} = Dam, the Computer was just better`);
+}
+
+
+
+function updateScore() {
+	if (alert === "`${currentGame} = Dam, the Computer was just better`") {
+		++pcScore;
+	} else {
+		++playerScore;
+	}
 }
