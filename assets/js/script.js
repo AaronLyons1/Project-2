8b8fc8fc5b65d29179398c54a9b975fa8abfb2b5
AaronLyons1 <lyonsaaron02@gmail.com>
2022-03-01 20:57:43 +0000

    added gamerChoice and pcChoice atributes, forgot about them, trying to run game but gitpod wont open a site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-let buttons = document.getElementsByClassName("btn")
-let pcScore = document.getElementsByClassName("pc-score")
-let playerImage = document.getElementById("ply-image")
-let pcImage = document.getElementById("pc-image")
-let playerScore = document.getElementsByClassName("ply-score")
-let options = ["paper","rock","scissors"]
+let buttons = document.getElementsByClassName("btn");
+let pcScore = document.getElementsByClassName("pc-score");
+let playerImage = document.getElementById("ply-image");
+let pcImage = document.getElementById("pc-image");
+let playerScore = document.getElementsByClassName("ply-score");
+let options = ["rock","paper","scissors"]
+const pcChoice = document.getAttribute("data-choice")
+const gamerChoice = document.getAttribute("data-choice")
 
 /**
  * Eventlisteners here!
@@ -23,18 +25,18 @@ function runGame(gamerChoice){
     playerImage.src = `assets/images/${options[gamerChoice]}`;
     playerImage.alt = options[gamerChoice];
 
-    let pcChoice = Math.floor(Math.random() * 3)
+    let pcChoice = Math.floor(Math.random() * 3);
 
     pcImage.src = `assets/images/${options[gamerChoice]}`;
     pcImage.alt = options[gamerChoice];
 
-    let result = checkWinner(options[pcChoice], options[playerChoice]);
+    let result = checkWinner(options[pcChoice], options[gamerChoice]);
 
     updateScore(result);
 }
 
 function checkWinner(){
-    let currentGame = `${pcChoice} against ${playerChoice}`;
+    let currentGame = `${pcChoice} against ${gamerChoice}`;
 
     //Check if the game is a tie
     if (pcScore === playerScore){
@@ -44,7 +46,7 @@ function checkWinner(){
 
 
     //rock
-    if(playerChoice === "rock"){
+    if(gamerChoice === "rock"){
         if(pcChoice === "scissors")
         alert(`${currentGame} = You Have Won! Yay`);
     } else {
@@ -52,7 +54,7 @@ function checkWinner(){
     }
 
     //paper
-    else if(playerChoice === "paper"){
+    if(gamerChoice === "paper"){
         if(pcChoice === "rock")
         alert(`${currentGame} = You Have Won! Yay`);
     } else {
@@ -60,7 +62,7 @@ function checkWinner(){
     }
 
     //scissors
-    else if (playerChoice === "scissors"){
+    if (gamerChoice === "scissors"){
         if(pcChoice === "paper")
         alert(`${currentGame} = You Have Won! Yay`);
     } else {
